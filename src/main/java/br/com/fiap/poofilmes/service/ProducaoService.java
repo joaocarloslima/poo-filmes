49e2e4fd238f0b1700c6050e29b9d7c92de304e0
joaocarloslima <joaocarloslima@me.com>
2024-10-31 21:21:04 -0300

    initial version

---
+++ src/main/java/br/com/fiap/poofilmes/service/ProducaoService.java
@@ -0,0 +1,12 @@
+package br.com.fiap.poofilmes.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducaoService {
+
+//    public int calcularAvaliacaoGeral(Classificavel classificavel){
+//        return classificavel.calcularAvaliacao();
+//    }
+
+}
